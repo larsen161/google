97ace8edac392746b0b681aa8030191c12cfc6ce
Steve <stevelarsen@gmail.com>
2020-08-15 03:06:33 -0500

    missing ; added

--- install-chrome.ps1
+++ install-chrome.ps1
@@ -1,4 +1,4 @@
 # This will run this code below directly in powershell
-# $ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/larsen161/google/master/install-chrome.ps1 Invoke-Expression $($ScriptFromGitHub.Content)
+# $ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/larsen161/google/master/install-chrome.ps1; Invoke-Expression $($ScriptFromGitHub.Content)
 
 $Path = $env:TEMP; $Installer = "chrome_installer.exe"; Invoke-WebRequest "http://dl.google.com/chrome/install/latest/chrome_installer.exe" -OutFile $Path\$Installer; Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait; Remove-Item $Path\$Installer

340a7a084626ee7e7dd3968e0d3d59c4d61f2271
Steve <stevelarsen@gmail.com>
2020-08-15 03:01:26 -0500

    install chrome on a windows machine quickly

---
+++ install-chrome.ps1
@@ -0,0 +1,1 @@
+$Path = $env:TEMP; $Installer = "chrome_installer.exe"; Invoke-WebRequest "http://dl.google.com/chrome/install/latest/chrome_installer.exe" -OutFile $Path\$Installer; Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait; Remove-Item $Path\$Installer

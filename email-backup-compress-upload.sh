80ef11d047d9e097d93a7d42304decb557b20ce8
Mike Lynch <mlynch@doximity.com>
2020-09-22 10:38:29 -0400

    Adding features for interactive input and bulk csv option

--- email-backup-compress-upload.sh
+++ email-backup-compress-upload.sh
@@ -12,23 +12,101 @@ today=`date +%Y-%m-%d`
 
 # Set the GYB variables
 # Set your gyb path and gybdata values. gybbase is default format used
-gyb=/home/steve/gyb/gyb
-gybbase=GYB-GMail-Backup-
-gybdata=/home/steve/gyb/GYB-GMail-Backup-
+GYB=/Users/$USER/bin/gyb/gyb
+GYBbase=GYB-GMail-Backup-
+GYBdata=/Users/$USER/GSuite-Backups/GYB-GMail-Backup-
 
 # Set the GAM variables
-# Change your user, gam path, shared drive ID
-user=steve
-gam=/home/steve/bin/gamadv-xtd3/gam
-shared_drive=0ABxUabcdefgUk9PVA
+# Change your gam path, shared drive ID, and GSuite admin user 
+GAM=~/bin/gamadv-xtd3/gam
+SHARED_DRIVE=1rcVk34XAQmMuWWyIe3dNTD2UCC-hKl6r
+ADMIN=service-account@domain.com
+
+# Check for $MAILBOX command line argument, otherwise prompt for mailbox
+check_mailbox() {
+	if [ -z $MAILBOX ]
+	then
+		get_mailbox
+    else 
+        return
+    fi
+}
+
+# Check for $CSV_FILE command line argument, otherwise prompt for csv location
+check_csv() {
+	if [ -z $CSV_FILE ]
+	then
+		get_csv
+    else 
+        return
+    fi
+}
+
+# Verify user account exists in Google Suite
+email_verification() {
+    if $GAM info user $MAILBOX >/dev/null 2>&1; 
+    then
+        return
+    else
+        echo -e "\033[0;31m$MAILBOX does not exist in your GSuite org.\033[0m"
+    fi
+    echo -e "\033[0;31mPlease enter a valid email address.\033[0m"
+    exit 1
+}
+
+# Capture the address of the mailbox to be backed up
+get_mailbox () {
+echo -n "Enter email address of the account you wish to backup & press Enter: "
+read -r MAILBOX
+}
+
+# Capture the location of the csv file
+get_csv () {
+echo -n "Enter full path of the csv file you wish to to backup & press Enter: "
+read -r CSV_FILE
+}
 
 # Backup, Compress, Upload files
-email=steve@domain.com
-$gyb --email $email
-7z u $gybdata$email.7z $gybdata$email
-$gam user $user add drivefile localfile $gybbase$email.7z teamdriveparentid $shared_drive teamdriveparentname $today
+backup_mailbox () {
+echo -e "\033[1;33m*** The email to be backed up is $MAILBOX ***\033[0m"
+$GYB --email $MAILBOX --local-folder $GYBdata$MAILBOX --service-account
+7z u $GYBdata$MAILBOX.7z $GYBdata$MAILBOX
+$GAM user $ADMIN add drivefile drivefilename $MAILBOX mimetype gfolder teamdriveparentid $SHARED_DRIVE
+$GAM user $ADMIN add drivefile localfile $GYBdata$MAILBOX.7z teamdriveparentname $MAILBOX
+}
 
-email=steve@another-domain.com
-$gyb --email $email
-7z u $gybdata$email.7z $gybdata$email
-$gam user $user add drivefile localfile $gybbase$email.7z teamdriveparentid $shared_drive teamdriveparentname $today
+
+PS3='Select your email backup preference: '
+options=('Single User' 'Bulk csv' 'Quit')
+select opt in "${options[@]}"
+do
+  case $opt in
+    'Single User')
+      check_mailbox
+      email_verification
+      backup_mailbox
+      break
+      ;;
+
+    'Bulk csv')
+      check_csv
+      OLDIFS=$IFS
+      IFS=','
+      [ ! -f $CSV_FILE ] && { echo -e "\033[0;31m$CSV_FILE file not found\033[0m"; exit 99; }
+      while read MAILBOX
+      do
+      	check_mailbox
+        email_verification
+        backup_mailbox
+      done < $CSV_FILE
+      IFS=$OLDIFS
+      break
+      ;;
+
+    'Quit')
+      break
+      ;;
+
+    *) echo invalid option;;
+  esac
+done

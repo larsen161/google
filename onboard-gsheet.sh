3c158118442a7a0909c6a25ba6cb2f68e851937c
Steve <stevelarsen@gmail.com>
2020-11-13 15:11:28 -0600

    Update onboard-gsheet.sh
    added additional notes so the script can be understood better

--- onboard-gsheet.sh
+++ onboard-gsheet.sh
@@ -2,8 +2,15 @@
 # Set the path to the GAM you want to use
 GAM=$HOME/bin/gamadv-xtd3/gam
 
-# Make a copy of this form and then get the sheet ID for the responses to update the ID value below
+# Make a copy of this form
 # https://docs.google.com/forms/d/1dng2Yud5v0-EAVnrJtEG_VK_1KjGIonLgk5DPtGOCKw/copy
+
+# Then get the sheet ID for the responses to update the ID value below
+# There are 3 additional columns that need to be added to the sheet
+# These are admin defined values which are used in the commands below
+# username = this is the username that makes up the users email. You define it as user or user@domain.com depending on your gam and domain setup
+# ou = what ou should the user be created in
+# onboarding = an email to notify your onboarding team along with the new hire after notify in the first command
 
 # Configure your organisation default and file info
 ADMIN=admin@domain.com
